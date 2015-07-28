8188ce092f8ccfed95c8ce625a81567909074d16
Dajia Bao <baod@uw.edu>
2015-07-28 13:28:49 -0700

    Changed cUrl to HTTP API to get eOutages page ITC-823

--- status-functions.php
+++ status-functions.php
@@ -3,21 +3,8 @@
 
 
 function check_e_outage() {
-  $options = Array(
-            CURLOPT_RETURNTRANSFER => TRUE,  // Setting cURL's option to return the webpage data
-            CURLOPT_FOLLOWLOCATION => TRUE,  // Setting cURL to follow 'location' HTTP headers
-            CURLOPT_AUTOREFERER => TRUE, // Automatically set the referer where following 'location' HTTP headers
-            CURLOPT_CONNECTTIMEOUT => 120,   // Setting the amount of time (in seconds) before the request times out
-            CURLOPT_TIMEOUT => 120,  // Setting the maximum amount of time for cURL to execute queries
-            CURLOPT_MAXREDIRS => 10, // Setting the maximum number of redirections to follow
-            CURLOPT_USERAGENT => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1a2pre) Gecko/2008073000 Shredder/3.0a2pre ThunderBrowse/3.2.1.8",  // Setting the useragent
-            CURLOPT_URL => E_OUTAGE_URL, // Setting cURL's URL option with the E_OUTAGE_URL variable passed into the function
-        );
 
-        $ch = curl_init();  // Initialising cURL
-        curl_setopt_array($ch, $options);   // Setting cURL's options using the previously assigned array data in $options
-        $data = curl_exec($ch); // Executing the cURL request and assigning the returned data to the $data variable
-        curl_close($ch);    // Closing cURL
+  $data = wp_remote_retrieve_body( wp_remote_get(E_OUTAGE_URL) );
 
 	$status = scrape_between($data, "<div class=\"status\">", "</body>");
 	if (strpos($data, "not operating normally")) {
