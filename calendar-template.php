c157f5dc3d52600932e9dc7b7b4ffc44e9a84135
Peter White <pewhite@uw.edu>
2017-04-18 15:44:45 -0700

    calendar-template.php

--- calendar-template.php
+++ calendar-template.php
@@ -67,7 +67,43 @@
 
 
     <div id='calendar'></div> 
-     
+    <p>
+    <h2>What information is on the IT Connect Change Calendar?</h2><p>
+    The IT Connect Change Calendar lists the approved UW-IT comprehensive and emergency changes. It also lists the "Make No Changes" days at the start of the quarter and any additional days approved by the UW-IT Enterprise change advisory board. The colors for the changes have no distinction just there to separate multiple events on a day. Make No Changes days are always in black.
+<br>
+Some examples of the approved changes include:
+<br><ul>
+<li>Planned network equipment software updates</li>
+<li>Changes to UW Connect</li>
+<li>Work being done in datacenters</li>
+<li>Azure changes</li></ul><p>
+
+<h2>Who do I ask if I don't know whether the entry should go on the IT Connect Change Calendar?</h2><p>
+                    Send an email to <script type="text/javascript">
+                      document.write("<a href=\"mail" + "to:" + new Array("help","uw.edu").join("@") + "\">" + "help@uw.edu" + "</" + "a>");
+                      </script> attention change management.<p>
+<h2>If the activity is not appropriate for the IT Connect Change Calendar are there other procedures that I should follow?</h2><p>
+                    There are other ways to communicate scheduled activities. Some examples include the <a href="https://www.washington.edu/cactools/teg/emaillistuse.html" target="_blank">"work in area", "cac services" and "outages" lists</a>. You should work with your team to determine all of the appropriate steps necessary to communicate your activity.
+<p>
+<h2>I'm trying to add an entry and I'm not authorized. How do I get permission to add?</h2><p>
+                    To request access contact <script type="text/javascript">
+                      document.write("<a href=\"mail" + "to:" + new Array("help","uw.edu").join("@") + "\">" + "help@uw.edu" + "</" + "a>");
+                      </script> attention change management.
+<p>
+<h2>What does "Make No Changes" mean?</h2><p>
+                    To insure quality service for students, faculty, and administrative staff during days marked with "Make No Changes":
+<br><ul>
+<li>Make no changes to production services</li>
+<li>Ensure full support coverage</li>
+<li>Perform extra monitoring</li>
+<li>Have a fallback / load contingency plan</li></ul><p>
+The peak load on UW Information Technology's computing and networking infrastructure typically occurs on the first day of the quarter, so the stability of all infrastructure components is critical. To minimize the chance of introducing a problem, no changes should be made to production equipment and services.
+<p>
+If an emergency change is needed, or the change involves routine maintenance that has no chance of adversely affecting service to campus, an exception can be made with the approval of the Vice President for UW Information Technology or a division leader via an emergency change submitted to the UW-IT Enterprise change advisory board.
+
+                                     
+
+
 </div>     
 </div>     
      
