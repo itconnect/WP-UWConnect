dd0a4bf1415efdf3faf4bb63c3242b12eb81a409
Nick Rohde <ncrohde@uw.edu>
2018-08-02 15:32:45 -0700

    modifying for new hero in header

--- service-home.php
+++ service-home.php
@@ -6,11 +6,26 @@
 
   <div class="row">
 
+    <div class="hero-container">
+
+      <?php uw_site_title(); ?>
+      <span class='udub-slant'><span></span></span>
+      <div class='uw-site-tagline' >Information technology tools and resources at the UW</div>
+
+      <div class="hero-search">
+        <form role="search" method="get" id="searchform" class="searchform" action="https://itconnect.uw.edu/">
+          <div>
+            <label class="screen-reader-text" for="s">Search IT Connect:</label>
+            <input type="text" value="" name="s" id="s" placeholder="Search IT Connect:" autocomplete="off">
+            <button type="submit" aria-label="Submit search" class="hero-search-submit"></button>
+          </div>
+        </form>
+      </div>
+
+    </div>
+
     <div class="col-md-<?php echo (($sidebar[0]!="on") ? "8" : "12" ) ?> 
          uw-content" role='main'>
-
-      <?php uw_site_title(); ?><span class="udub-slant"><span></span></span><h3 class="uw-site-tagline" >Information technology tools and resources at the UW</h3>
-
 
       <?php uw_mobile_front_page_menu(); ?>
       
