1736a9558742c091ab0cbc348ef5f82258e71f69
Nick Rohde <ncrohde@uw.edu>
2019-08-27 09:35:04 -0700

    Updating templae for services

--- single-service.php
+++ single-service.php
@@ -24,97 +24,36 @@ get_header(); ?>
 
     </div>
 
-    <div class="col-md-<?php echo (($sidebar[0]!='on') ? '8' : '12' ) ?>
-         uw-content" role='main'>
 
-      <?php uw_mobile_front_page_menu(); ?>
+<!-- Moved message -->
 
-      <?php service_breadcrumbs(); ?>
+        <div class="row show-grid">
+          
+          <div class="col-md-12">
 
-      <div id='main_content' class="uw-body-copy" tabindex="-1">
-             <?php while (have_posts()) : the_post();
-                global $post;
-                $id = $post->ID;
-                ?>
-                <h1 class='entry-title'><?php the_title(); ?></h1>
-                <?php if (get_post_meta($id, 'uwc-description', true)) { ?>
-                  <div class='attr-wrap'>
-                    <h2 class='service-attr'>Service Description</h2>
-                    <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-description', true)); ?></div>
-                  </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-options-text', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Service Options</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-options-text', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-eligibility', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Eligibility</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-eligibility', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-ordering', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>How to Order</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-ordering', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-availability', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Availability</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-availability', true)); ?></div>
-               </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-price', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Price</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-price', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-level-descr', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Service Level Description</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-level-descr', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-additional-info', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Additional Information</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-additional-info', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-support-info', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Support Information</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-support-info', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-customer-ref', true)) { ?>
-                <div class='attr-wrap'>
-                  <h2 class='service-attr'>Contact for More Information</h2>
-                  <div class='attr-text'><?php echo wpautop(get_post_meta($id, 'uwc-customer-ref', true)); ?></div>
-                </div>
-                <?php } ?>
-                <?php if (get_post_meta($id, 'uwc-last-review', true)) { ?>
-                <hr id="fold" />
-                <div class='superattr-wrap'>
-                  <h2 class='service-attr belowf'>Maintenance</h2>
-                  <div class='subattr-wrap'>
-                    <p class='attr-text'>Last Review Date: <?php echo get_post_meta($id, 'uwc-last-review', true); ?></p>
-                  </div>
-                </div>
-                <?php if (current_user_can('edit_posts')) {
-                    edit_post_link('Edit', '<p>', '</p>');
-                } ?>
-                <?php } ?>
-                <?php endwhile; ?>
-       </div> <!-- main_content -->
-     </div> <!-- uw-content -->
-     <div id='sidebar' role='navigation' aria-label='Sidebar Menu'>
-          <?php dynamic_sidebar('Service-Catalog-Sidebar'); ?>
-     </div> <!-- #sidebar -->
+            <div class="row show-grid">
+
+              <div class="col-md-6">
+                
+                <h1>The UW-IT Service Catalog has moved.</h1>
+                <p>The UW-IT Service Catalog has moved to a new location. Please update your links and bookmarks to point to <a href="https://uw.service-now.com/sp?id=sc_home">https://uw.service-now.com/sp?id=sc_home</a></p>
+                <p></p>
+              
+              </div>
+
+              <div class="col-md-5 col-md-offset-1">
+
+                <div class="woof" style="background: url( <?php echo get_template_directory_uri() . '/assets/images/404.jpg' ?>) center center no-repeat; background-size: 100%;"></div>
+                
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+<!-- End Moved Message -->
+
     </div> <!-- row -->
   </div> <!-- uw-body -->
 <?php get_footer(); ?>
